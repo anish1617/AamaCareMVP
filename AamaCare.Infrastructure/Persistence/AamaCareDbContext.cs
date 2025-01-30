da3912fcd77739dec44a41b2ff52b5c3a346a56d
Anish Mandal <anishmandal1617@gmail.com>
2025-01-30 23:33:29 +0545

    feat: DbContext and PregnancyRepository implemented

--- AamaCare.Infrastructure/Persistence/AamaCareDbContext.cs
+++ AamaCare.Infrastructure/Persistence/AamaCareDbContext.cs
@@ -1,5 +1,27 @@
-﻿namespace AamaCare.Infrastructure.Persistence;
+﻿using AamaCare.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-public class AamaCareDbContext
+namespace AamaCare.Infrastructure.Persistence;
+
+public class AamaCareDbContext :DbContext
 {
+    public AamaCareDbContext(DbContextOptions<AamaCareDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        
+    }
+
+    public DbSet<Pregnancy> Pregnancies { get; set; }
+
 }
