0b6743346127a42481f6670d3877237c064d8b70
Anish Mandal <anishmandal1617@gmail.com>
2025-01-31 00:55:44 +0545

    add src folder

---
+++ src/AamaCare.Application/Features/PregnancyTracker/CreatePregnancy/CreatePregnancyValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace AamaCare.Application.Features.PregnancyTracker.CreatePregnancy
+{
+    public class CreatePregnancyValidator : AbstractValidator<CreatePregnancyCommand>
+    {
+        public CreatePregnancyValidator()
+        {
+            RuleFor(x => x.StartDate).NotEmpty().LessThan(DateTime.Now);
+        }
+    }
+}
---
+++ src/AamaCare.Infrastructure/Persistence/AamaCareDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AamaCare.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AamaCare.Infrastructure.Persistence;
+
+public class AamaCareDbContext :DbContext
+{
+    public AamaCareDbContext(DbContextOptions<AamaCareDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        
+    }
+
+    public DbSet<Pregnancy> Pregnancies { get; set; }
+
+}
