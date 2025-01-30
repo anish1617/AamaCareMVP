8e25bd77afef79e654ede8764a94e9f0b0c2dd90
Anish Mandal <anishmandal1617@gmail.com>
2025-01-30 23:22:24 +0545

    feat: setup validator, command and queries for pregnancy tracker features

---
+++ AamaCare.Application/Features/PregnancyTracker/Queries/GetPregnancyByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using AamaCare.Domain.Entities;
+using MediatR;
+
+namespace AamaCare.Application.Features.PregnancyTracker.Queries
+{
+    public class GetPregnancyByIdQuery : IRequest<Pregnancy>
+    {
+        public Guid Id { get; set; }
+    }
+}
